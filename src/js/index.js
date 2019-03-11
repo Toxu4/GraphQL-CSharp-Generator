f60346c0ad1577a3e984624600d6ff928b324566
Антон Красильщиков <a.krasilshchikov@rambler-co.ru>
2019-03-11 18:29:10 +0300

    code cleanup

--- src/js/index.js
+++ src/js/index.js
@@ -1,59 +1,66 @@
 #!/usr/bin/env node
-console.log(__dirname)
 
-let program = require('commander');
+let args = parseCommandArgs();
+let [tempConfigFileName, cleanupTempFile] = createTempConfig(args);
 
-program
-    .option('-s, --schema <schema>', 'path to json with schema, or schema url. e.g. -s ./schema.json')
-    .option('-d, --documents <documents>', 'path with to documents with queries. e.g. -d ./Queries/*.graphql')
-    .option('-o, --output <output>', 'output file name. e.g. -o ./Generated.cs')
-    .option('-n, --namespace <namespace>', 'namespace of generated file. e.g. -n MyGraphQlClient')
-    .parse(process.argv);
+let cmd = require('os').platform() === 'win32' ? 'node.exe' : 'node';
 
-const fs = require('fs');
-const Handlebars = require('Handlebars');
+require("child_process")
+    .spawn(
+        cmd, 
+        [`${__dirname}\\node_modules\\graphql-code-generator\\dist\\cli.js`,'--config', tempConfigFileName], 
+        { stdio: 'inherit'})
+    .on('exit', function() {
+        cleanupTempFile();
+    });
 
-const configTemplate = fs.readFileSync(`${__dirname}/Templates/config.handlebars`);
-const compile = Handlebars.compile('' + configTemplate);
+function parseCommandArgs(){
+    let args = require('commander');
 
-if (!program.schema){
-    console.error("schema expected");
-    process.exit(1);    
+    args
+        .option('-s, --schema <schema>', 'path to json with schema, or schema url. e.g. -s ./schema.json')
+        .option('-d, --documents <documents>', 'path with to documents with queries. e.g. -d ./Queries/*.graphql')
+        .option('-o, --output <output>', 'output file name. e.g. -o ./Generated.cs')
+        .option('-n, --namespace <namespace>', 'namespace of generated file. e.g. -n MyGraphQlClient')
+        .parse(process.argv);
+
+    if (['schema','documents','output','namespace'].map(expect).some(exists => !exists)) {
+        console.log();
+        args.outputHelp();
+
+        process.exit(1);
+    }
+    
+    // o-o-ops
+    args.pluginDir = __dirname;
+
+    return args;
+
+    function expect(key){
+        if (!args[key]){            
+            console.error(`${key} expected`);
+            return false;
+        }
+        return true;
+    }
 }
 
-if (!program.documents){
-    console.error("documents expected");
-    process.exit(1);    
+function createTempConfig(args){   
+    var tempConfigFileName = require('tmp').tmpNameSync({ prefix: 'gen-csharp-config-', postfix: '.yml'});
+
+    const fs = require('fs');
+
+    const configTemplate = fs.readFileSync(`${__dirname}/Templates/config.handlebars`);
+    const compile = require('Handlebars').compile('' + configTemplate);
+    
+    let fd = fs.openSync(tempConfigFileName, 'a');
+    fs.appendFileSync(tempConfigFileName, compile(args), 'utf8');
+    fs.closeSync(fd);
+
+    return [
+        tempConfigFileName, 
+        function(){ 
+            fs.unlinkSync(tempConfigFileName); 
+        }
+    ]
 }
-
-if (!program.output){
-    console.error("output expected");
-    process.exit(1);    
-}
-
-if (!program.namespace){
-    console.error("namespace expected");
-    process.exit(1);    
-}
-
-var tempConfigFileName = require('tmp').tmpNameSync({ prefix: 'gen-csharp-config-', postfix: '.yml'});
-
-program.pluginDir = __dirname;
-
-let fd = fs.openSync(tempConfigFileName, 'a');
-fs.appendFileSync(tempConfigFileName, compile(program), 'utf8');
-fs.closeSync(fd);
-
-const os = require('os');
-const spawn = require("child_process").spawn;
-
-let cmd = os.platform() === 'win32' ? 'node.exe' : 'node';
-
-let gen = spawn(cmd, [`${__dirname}\\node_modules\\graphql-code-generator\\dist\\cli.js`,'--config', tempConfigFileName], {stdio: 'inherit'})
-gen.on('error', function(err) {
-    console.error(err);
-    process.exit(1);
-});
-gen.on('exit', function() {
-    fs.unlinkSync(tempConfigFileName);
-});
