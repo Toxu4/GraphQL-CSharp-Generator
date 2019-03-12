079d5832d55e979d89b1420ee5231395ae3def62
Антон Красильщиков <a.krasilshchikov@rambler-co.ru>
2019-03-12 18:23:11 +0300

    cs lib

---
+++ src/cs/Toxu4.GraphQl.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Toxu4.GraphQl.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
